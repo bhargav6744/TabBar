64fd5bc783dbcf8d431374ee9d46d19dddd92625
Bhargav Sejpal <sejpalbhargav67@gmail.com>
2017-09-20 19:01:54 +0530

    TabBar

---
+++ tabbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tabbar
+//
+//  Created by Hitesh V-PI on 30/05/17.
+//  Copyright © 2017 Pixabit Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
